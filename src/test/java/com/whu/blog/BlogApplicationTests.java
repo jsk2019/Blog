9e97cd869ed3d940e5c15e96da9452b1fc1d6576
jsk2019 <530578697@qq.com>
2020-05-12 00:07:09 +0800

    create

---
+++ src/test/java/com/whu/blog/BlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whu.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
